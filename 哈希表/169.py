4b89311b3952efe62d5f62d5ef7046e0b9715b3e
Ashley <163224217+DDAshley126@users.noreply.github.com>
2025-09-30 11:52:27 +0800

    Delete 哈希表 directory

--- 哈希表/169.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-        n = len(nums)
-        times = dict()
-        for i in nums:
-            times[i] = times.get(i,0) + 1
-        for i in set(nums):
-            if times.get(i) > (n//2):
-                return i
